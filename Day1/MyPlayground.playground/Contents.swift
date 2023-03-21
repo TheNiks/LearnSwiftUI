4cc6ede4ae21639fb57439c5af8f6cfd197948f8
Nikunj <nikunjmodi86@gmail.com>
2023-03-21 12:10:38 +0530

    [Day-1] Practice some code from Hacking course SwiftUI.

---
+++ Day1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,71 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+/// Day 1
+///
+var name = "Nikunj"
+name = "Modi Nikunj"
+name = "Modi Nikunj K."
+
+let city = "Danta"
+
+var playerName = "Modi"
+print(playerName)
+
+playerName = "Nikunj"
+print(playerName)
+
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+/// Multiline code
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+print(result.uppercased())
+
+/// How to store whole numbers.
+let score = 10
+let reallyBig = 100_00_00
+
+let lowerScore = score - 2
+let higerScore = score + 5
+let doubleScore = score * 2
+let squareScore = score * score
+let halfScore = score / 2
+
+var counter = 10
+counter += 5
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
+
+
+/// How to store decimal numbers.
+let number1 = 0.1 + 0.3
+print(number1)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var rating = 5.0
+rating *= 2
+
+
